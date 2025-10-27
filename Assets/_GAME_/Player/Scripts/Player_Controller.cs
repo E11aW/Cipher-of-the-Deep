f7c3de861a473e75c07bfec9bdc706269d4c2aaf
ClaytonMcArthur <claxx0506@icloud.com>
2025-10-26 21:52:50 -0700

    Adding game save system for player position in a JSON file for future expandability

--- Assets/_GAME_/Player/Scripts/Player_Controller.cs
+++ Assets/_GAME_/Player/Scripts/Player_Controller.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [SelectionBase]
-
 public class Charactar_Controller : MonoBehaviour
 {
     #region Editor Data
@@ -18,7 +15,20 @@ public class Charactar_Controller : MonoBehaviour
     private Vector2 _moveDir = Vector2.zero;
     #endregion
 
-    #region Tick
+    // -------------------- NEW: LOAD on start --------------------
+    private void Awake()
+    {
+        var data = SaveSystem.Load();
+        if (data != null)
+        {
+            var pos = data.ToVector2();
+            if (_rb != null)
+                _rb.position = pos; // physics-safe
+            else
+                transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        }
+    }
+
     private void Update()
     {
         GatherInput();
@@ -28,20 +38,46 @@ public class Charactar_Controller : MonoBehaviour
     {
         MovementUpdate();
     }
-    #endregion
 
-    #region Input Logic
+    // -------------------- NEW: SAVE on pause/quit --------------------
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus) SaveNow();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveNow();
+    }
+
+    private void SaveNow()
+    {
+        Vector2 pos = _rb != null ? _rb.position : (Vector2)transform.position;
+        // If you have multiple scenes, pass the scene name:
+        // var sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        var data = SaveData.FromPosition(pos /*, sceneName*/);
+        SaveSystem.Save(data);
+    }
+
     private void GatherInput()
     {
         _moveDir.x = Input.GetAxisRaw("Horizontal");
         _moveDir.y = Input.GetAxisRaw("Vertical");
     }
-    #endregion
 
-    #region Movement Logic
     private void MovementUpdate()
     {
-        _rb.linearVelocity = _moveDir * _moveSpeed * Time.fixedDeltaTime;
+        // If your Unity doesn't have linearVelocity, use _rb.velocity
+        if (_rb != null)
+            _rb.linearVelocity = _moveDir * _moveSpeed * Time.fixedDeltaTime;
+        else
+            transform.position += (Vector3)(_moveDir * _moveSpeed * Time.fixedDeltaTime);
     }
-    #endregion
+
+    // Handy for testing (Right-click component header -> “Reset Saved Position”)
+    [ContextMenu("Reset Saved Position")]
+    private void ResetSavedPosition()
+    {
+        SaveSystem.Delete();
+    }
 }
